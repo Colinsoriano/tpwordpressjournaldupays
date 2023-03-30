6d6ece300a10ca054b1e17cf3bb91b15e932e3b5
Colin <112839508+Colinsoriano@users.noreply.github.com>
2023-03-31 09:35:56 +1100

    Delete tpjournal directory

--- tpjournal/footer.php
+++
@@ -1,8 +0,0 @@
- </div>
- <footer class=" navbar navbar-expand-lg bg-body-tertiary bg-primary navbar-dark">
-     <?php wp_nav_menu(['theme_location' => 'footer', 'container' => false, 'menu_class' => 'navbar-nav mr-auto']) ?>
- </footer>
- <?php wp_footer() ?>
- </body>
-
- </html>
